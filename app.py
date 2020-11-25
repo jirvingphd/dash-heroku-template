c1c34123bf98abc55c8bcf663b6b2a7ca85b2397
James M. Irving, Ph.D <james.irving@flatironschool.com>
2020-11-25 11:04:46 -0500

    Update app.py for dash v1.17.0

--- app.py
+++ app.py
@@ -2,23 +2,21 @@
 import os
 from random import randint
 
-import plotly.plotly as py
-from plotly.graph_objs import *
-
 import flask
 import dash
-from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
-
+import plotly.express as px
+import pandas as pd
 
 # Setup the app
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
+
 server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
 
+app = dash.Dash(__name__, server=server)
 
 # Put your Dash code here
 
